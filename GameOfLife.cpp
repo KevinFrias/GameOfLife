f82764645b546e6317ad556177aa106fe1036384
Kevin F <knightkevin99@gmail.com>
2023-03-24 21:43:50 -0600

    Arreglo al momento de creacion de los diferentes threads

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
+#include <chrono>
 using namespace std;
 
 const int n = 5, m = 5;
 vector < vector <char>> matrix(n, vector(m, 'a'));
+vector < vector <char>> matrix_clean(n, vector(m, 'a'));
 vector < vector <char>> matrix_next_gen(n, vector(m, 'a'));
 
 // Matrix[n][m] -> Matrix[y][x] -> Matrix[j][i]
@@ -10,11 +12,10 @@ vector < vector <char>> matrix_next_gen(n, vector(m, 'a'));
 
 void next_gen(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 	
-	if (iteracion == 0){
+	if (iteracion == 1){
 		for (int i = x1; i <= x2; i++){
 			for (int j = y1; j <= y2; j++) {
                 int poblacion = 0;
-
                 if (nulo){
 					// Lado izquierdo
 					if (i - 1 >= 0){
@@ -34,12 +35,11 @@ void next_gen(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 					}
                 }
 				else{
-					// Arriba
-                    int indexArriba = (j - 1 < 0 ? n - 1 : j - 1);
-                    int indexAbajo = (j + 1 >= n ? 0 : j + 1);
-
                     int indexIzquierda = (i - 1 < 0 ? m - 1 : i - 1);
                     int indexDerecha = (i + 1 >= m ? 0 : i + 1);
+                    
+					int indexArriba = (j - 1 < 0 ? n - 1 : j - 1);
+                    int indexAbajo = (j + 1 >= n ? 0 : j + 1);
 
 					// Lado inzquierdo
 					poblacion += ((matrix[indexArriba][indexIzquierda] - 'a')&1);
@@ -55,6 +55,9 @@ void next_gen(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 					poblacion += ((matrix[indexArriba][indexDerecha] - 'a')&1);
 					poblacion += ((matrix[j][indexDerecha] - 'a')&1);
 					poblacion += ((matrix[indexAbajo][indexDerecha] - 'a')&1);
+
+					//cout << j << ", " << i << "  |  " << indexArriba << " - " << indexAbajo << " - ";
+					//cout << indexIzquierda << " - " << indexDerecha << "  |  " << poblacion << endl;
 
 				}
 
@@ -73,28 +76,32 @@ void next_gen(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 	int mitad_x = x2 / 2;
 	int mitad_y = y2 / 2;
 
-	thread cuadrantes_1 (next_gen, x1, mitad_x, y1, mitad_y, iteracion + 1, true);
-	thread cuadrantes_2 (next_gen, mitad_x  + 1, x2, y1, mitad_y, iteracion + 1, true);
-	thread cuadrantes_3 (next_gen, x1, mitad_x, mitad_y  + 1, y2, iteracion + 1, true);
-	thread cuadrantes_4 (next_gen, mitad_x + 1, x2, mitad_y + 1, y2, iteracion + 1, true);
+	thread cuadrantes[4];
+	
+	cuadrantes[0] = thread (next_gen, x1, mitad_x, y1, mitad_y, iteracion + 1, nulo);
+	cuadrantes[1] = thread (next_gen, mitad_x  + 1, x2, y1, mitad_y, iteracion + 1, nulo);
+	cuadrantes[2] = thread (next_gen, x1, mitad_x, mitad_y  + 1, y2, iteracion + 1, nulo);
+	cuadrantes[3] = thread (next_gen, mitad_x + 1, x2, mitad_y + 1, y2, iteracion + 1, nulo);
 
-	cuadrantes_1.join();
-	cuadrantes_2.join();
-	cuadrantes_3.join();
-	cuadrantes_4.join();
 
+	for (int i = 0; i < 4; i++) cuadrantes[i].join();
 
 	return;
 }
 
 
 void imprimir(){
+	system ("clear");
 	cout << endl;
 	for (int i = 0; i < n; i++){
-		for (int j = 0; j < m; j++) cout << (matrix[i][j]-'a') << " ";
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] == 'a') cout << "- ";
+			else cout << "* ";
+		}
 		cout << endl;
 	}
-	cout << endl << endl;
+	cout << flush << endl;
+	sleep(1);
 }
 
 
@@ -107,12 +114,14 @@ int main(){
 	matrix[2][1] = 'b';
 	matrix[2][2] = 'b';
 
+	int a = 25;
 
-	imprimir();
-	next_gen(0, n-1, 0, m-1, 0, true);    
-	matrix = matrix_next_gen;
-	imprimir();
-
+	while(a--){
+		imprimir();
+		next_gen(0, n-1, 0, m-1, 0, false);    
+		matrix = matrix_next_gen;
+		matrix_next_gen = matrix_clean;
+	}
 
 	return 0;
 }
