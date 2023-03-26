5f73ffb845801919523f1c9720045f94a4a44179
Kevin F <knightkevin99@gmail.com>
2023-03-25 21:31:14 -0600

    La visualizacion de una pantalla demo del juego es visible

--- Basic2Window.cpp
+++ Basic2Window.cpp
@@ -2,7 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n = 20, m = 20;
+vector < vector <char>> matrix(n, vector(m, 'a'));
+vector < vector <char>> matrix_clean(n, vector(m, 'a'));
+vector < vector <char>> matrix_next_gen(n, vector(m, 'a'));
+
 int main() {
+
+    // Glider
+	matrix[0][1] = 'b';
+	matrix[1][2] = 'b';
+	matrix[2][0] = 'b';
+	matrix[2][1] = 'b';
+	matrix[2][2] = 'b';
+
+
 
     sf::RenderWindow outerWindow(sf::VideoMode(1650, 810), "Outer Window");
 
@@ -11,11 +25,15 @@ int main() {
 
     sf::Sprite innerSprite(innerTexture.getTexture());
 
-    int distancia_X = (outerWindow.getSize().x - innerTexture.getSize().x);
+    int distancia_X = (outerWindow.getSize().x - innerTexture.getSize().x) - 40;
     int distancia_Y = (outerWindow.getSize().y - innerTexture.getSize().y) / 2;
 
+    innerSprite.setPosition(sf::Vector2f(distancia_X, distancia_Y));
 
-    innerSprite.setPosition(sf::Vector2f(distancia_X - 40, distancia_Y));
+
+    int sizeCelda_X = innerTexture.getSize().x / m , sizeCelda_Y = innerTexture.getSize().y / n;
+
+
 
     while (outerWindow.isOpen()) {
         sf::Event event;
@@ -26,20 +44,47 @@ int main() {
             }
         }
 
+
+        // Empezamos a declarar la forma que va a tomar las celdas dentro del juego de la vida
+
+//--- El tama;o de la celda va a cambiar conforme se tenga mas o menos zoom
+
+        sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
+        celda.setOutlineColor(sf::Color::White);
+        celda.setOutlineThickness(2.f);
+
         innerTexture.clear(sf::Color::Black);
+        outerWindow.clear(sf::Color(0x808080));
 
-        // Draw stuff onto the inner window
-        // ...
+// -- Aqui hay que dividir el poner todas las celdas que esten encendidas dentor del innerWindow
+//-- Tenemos que hcaer udo de multithreading
 
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] == 'b'){
+                    celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
+                    celda.setFillColor(sf::Color::White);
+                    innerTexture.draw(celda);
+                }
+                /*
+                else {
+                    celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
+                    celda.setFillColor(sf::Color::Black);
+                    innerTexture.draw(celda);
+                }
+                */
+            }
+        }
+
+
+        // Mostramos la ventana donde mostramos el juego de la vida
         innerTexture.display();
 
-        // Asignamos un color de fondo para la pantalla del fondo, este caso, Negro
-        outerWindow.clear(sf::Color(0x808080));
-        // Draw the inner window onto the outer window
+        // Colocamos la ventana donde se muestra el juego, dentro de la ventana donde 
+        // se colocó todas las opciones de visualizacion
         outerWindow.draw(innerSprite);
         outerWindow.display();
     }
 
     return 0;
 }
-
