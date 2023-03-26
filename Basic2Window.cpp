a11088a47faeba9cc8fbdf89513c3fa5601eb5b7
Kevin F <knightkevin99@gmail.com>
2023-03-26 12:30:03 -0600

    Se agregaron comentarios descriptivos e implementacion de funcion para dibujar las celdas

--- Basic2Window.cpp
+++ Basic2Window.cpp
@@ -2,10 +2,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n = 20, m = 20;
+int n = 140, m = 140;
+int MAXN = 350, MAXM = 350;
 vector < vector <char>> matrix(n, vector(m, 'a'));
 vector < vector <char>> matrix_clean(n, vector(m, 'a'));
 vector < vector <char>> matrix_next_gen(n, vector(m, 'a'));
+
+
+void prueba(sf::RenderTexture& innerTexture, int sz_x, int sz_y, sf::RectangleShape& celda, mutex &mutex){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (matrix[i][j] == 'a'){
+                celda.setPosition(i*sz_x, j*sz_y);
+                celda.setFillColor(sf::Color(255,191,0));
+                innerTexture.draw(celda);
+                /*
+                lock_guard<std::mutex> lock(mutex);
+                innerTexture.draw(celda);
+                lock_guard<std::mutex> unlock(mutex);
+                */
+            }
+        }
+    }
+
+    return;
+}
+
+
 
 int main() {
 
@@ -17,74 +40,82 @@ int main() {
 	matrix[2][2] = 'b';
 
 
-
-    sf::RenderWindow outerWindow(sf::VideoMode(1650, 810), "Outer Window");
-
+    // Creamos dos ventanas, la primera para colocar el juego y la segunda para colocar la primera ventana y los botones para las opciones
+    sf::RenderWindow outerWindow(sf::VideoMode(1650, 860), "Conway's Game of Life");
     sf::RenderTexture innerTexture;
-    innerTexture.create(1200, 710);
+    innerTexture.create(1400, 700);
 
+    // Limpiamos la pantalla principal y le colocamos un color de fondo
+    outerWindow.clear(sf::Color(92,117,140));
+
+    // Calculamos la altura y la anchura de las celdas dependiendo de cuantas celdas se quieran ver
+    int sizeCelda_X = innerTexture.getSize().x / m , sizeCelda_Y = innerTexture.getSize().y / n;
+    // Creamos la figura con las medidades anteriores
+    sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
+
+
+    // Usamos la clase Sprite para poder dibujar todas las celdas dentro de la pantalla del juego
     sf::Sprite innerSprite(innerTexture.getTexture());
 
-    int distancia_X = (outerWindow.getSize().x - innerTexture.getSize().x) - 40;
-    int distancia_Y = (outerWindow.getSize().y - innerTexture.getSize().y) / 2;
+    // Calculamos la posicion para poder colocar correctamente la pantalla
+    int distancia_X = (outerWindow.getSize().x - innerTexture.getSize().x) - 5;
+    int distancia_Y = ((outerWindow.getSize().y - innerTexture.getSize().y) / 2);
 
+    // Posicionamos la ventana del juego
     innerSprite.setPosition(sf::Vector2f(distancia_X, distancia_Y));
 
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//  BOTONES
+    // Primero creamos la forma que van a tener los diferentes botones
+    sf::RectangleShape button(sf::Vector2f(100.0f, 50.0f));
+    button.setFillColor(sf::Color(77,0, 255));
 
-    int sizeCelda_X = innerTexture.getSize().x / m , sizeCelda_Y = innerTexture.getSize().y / n;
+    // Definimos la fuente que vamos a ocupar dentro de la ventana
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
 
+    // Creamos el texto para cada uno de los botones
+    sf::Text buttonText("Prueba", font, 22);
+    buttonText.setFillColor(sf::Color::White);
 
+    // Posicionamos el boton y su texto correspondiente
+    button.setPosition(100.0f, 225.0f);
+    buttonText.setPosition(115.0f, 240.0f);
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    // Creamos un mutex para que al momento que ocupamos los diferentes hilos, no se pierda la informacion de las celdas que estamos dibujando
+    mutex bloqueo;
+
+    // Bucle que ocupamos para la pantalla mientras ésta esté presente
     while (outerWindow.isOpen()) {
         sf::Event event;
 
+        // Checamos si es que en algun momento se tuvo algun evento
         while (outerWindow.pollEvent(event)) {
+            // En caso de que el evento sea cerrar la ventana, cerramos completamente toda la ventana
             if (event.type == sf::Event::Closed){
                 outerWindow.close();
             }
         }
 
-
-        // Empezamos a declarar la forma que va a tomar las celdas dentro del juego de la vida
-
-//--- El tama;o de la celda va a cambiar conforme se tenga mas o menos zoom
-
-        sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
-        celda.setOutlineColor(sf::Color::White);
-        celda.setOutlineThickness(2.f);
-
+        // Para la ventana donde presentamos el juego, ponemos un fondo de color
         innerTexture.clear(sf::Color::Black);
-        outerWindow.clear(sf::Color(0x808080));
 
-// -- Aqui hay que dividir el poner todas las celdas que esten encendidas dentor del innerWindow
-//-- Tenemos que hcaer udo de multithreading
+        // Funcion de ayuda para calcular y dibujar las celdas del juego
+        prueba(innerTexture, sizeCelda_X, sizeCelda_Y, celda, bloqueo);
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (matrix[i][j] == 'b'){
-                    celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
-                    celda.setFillColor(sf::Color::White);
-                    innerTexture.draw(celda);
-                }
-                /*
-                else {
-                    celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
-                    celda.setFillColor(sf::Color::Black);
-                    innerTexture.draw(celda);
-                }
-                */
-            }
-        }
-
-
-        // Mostramos la ventana donde mostramos el juego de la vida
-        innerTexture.display();
-
-        // Colocamos la ventana donde se muestra el juego, dentro de la ventana donde 
-        // se colocó todas las opciones de visualizacion
+        // Colocamos en la ventana principal, la ventana del juego
         outerWindow.draw(innerSprite);
+        
+        // Colocamos los diferentes botones
+        outerWindow.draw(button);
+        outerWindow.draw(buttonText);
+
+        // Mostramos la pantalla con todos los elementos que colocamos anteriormente
         outerWindow.display();
+        innerTexture.display();
     }
 
     return 0;
 }
+
