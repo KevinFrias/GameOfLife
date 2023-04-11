052f68b41b702d416796562f9e20294f5527d988
Kevin F <knightkevin99@gmail.com>
2023-04-10 22:32:58 -0600

    Reestructuracion del manejo para el dibujo de las celdas vivas e implemtacion de la funcionalidad de siguiente evolucion

--- Basic2Window.cpp
+++ Basic2Window.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <bits/stdc++.h>
 #include <semaphore.h>
+#define PB push_back
 using namespace std;
 
 int n = 50;
@@ -9,6 +10,9 @@ int sizeCelda_X, sizeCelda_Y;
 vector < vector <bool>> matrix(n, vector(n, false));
 vector < vector <bool>> matrix_clean(n, vector(n, false));
 vector < vector <bool>> matrix_next_gen(n, vector(n, false));
+
+list <pair<int,int>> live_cells;
+
 
 int color_muerto[] = {0,0,0};
 int color_vivo[] = {255,255,255};
@@ -24,9 +28,9 @@ sf::Font font;
 sem_t sem;
 mutex bloqueo;
 
-
-// Creamos la ventana del juego
 sf::RenderTexture inner;
+
+
 
 pair<sf::RectangleShape, sf::Text> createButton(int szBtnX, int szBtny, int posX, int posY, string texto, int szTexto, int posTX, int posTY){
     sf::RectangleShape button(sf::Vector2f(szBtnX, szBtny));
@@ -43,10 +47,6 @@ pair<sf::RectangleShape, sf::Text> createButton(int szBtnX, int szBtny, int posX
 void handleNextStep(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 
 	if (iteracion == 0){
-        // Creamos la forma de la celda
-        sizeCelda_X = zoom[index_zoom];
-        sizeCelda_Y = zoom[index_zoom];
-        sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
 
 		for (int i = x1; i <= x2; i++){
 			for (int j = y1; j <= y2; j++) {
@@ -96,19 +96,15 @@ void handleNextStep(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 
 				if (matrix[j][i]){
 					if (poblacion >= 4 || poblacion <= 1)  matrix_next_gen[j][i] = false;
-					else{ 
-                        celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
-                        celda.setFillColor(sf::Color(color_vivo[0], color_vivo[1], color_vivo[2]));
-                        inner.draw(celda);
+					else{
+                        live_cells.PB({j,i});
                         matrix_next_gen[j][i] = true;
                     }
                 }
 
 				else {
 					if (poblacion == 3){
-                        celda.setPosition(i*sizeCelda_X, j*sizeCelda_Y);
-                        celda.setFillColor(sf::Color(color_vivo[0], color_vivo[1], color_vivo[2]));
-                        inner.draw(celda);
+                        live_cells.PB({j,i});
                         matrix_next_gen[j][i] = true;
                     }
                 }
@@ -119,6 +115,7 @@ void handleNextStep(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
 
 		return;
 	}
+
     int mitad_x = x2 / 2;
 	int mitad_y = y2 / 2;
 
@@ -134,47 +131,36 @@ void handleNextStep(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
     return;
 }
 
-void updateGameVisual(int x1, int x2, int y1, int y2, int iteracion, bool nulo){
+void updateGameVisual(){
 
-	if (iteracion == 0){
-        // Creamos la forma de la celda
-        sizeCelda_X = zoom[index_zoom];
-        sizeCelda_Y = zoom[index_zoom];
-        static sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
+    sizeCelda_X = zoom[index_zoom];
+    sizeCelda_Y = zoom[index_zoom];
+    static sf :: RectangleShape celda(sf::Vector2f(sizeCelda_X, sizeCelda_Y));
 
-		for (int i = x1; i <= x2; i++){
-			for (int j = y1; j <= y2; j++) {
-                
-                sem_wait(&sem);
+    for (list<pair<int,int>>:: iterator it = live_cells.begin(); it != live_cells.end(); it++){
+        int a = it -> first;
+        int b = it -> second;
+        celda.setPosition(a*sizeCelda_Y, b*sizeCelda_Y);
+        celda.setFillColor(sf::Color(color_vivo[0], color_vivo[1], color_vivo[2]));
+        inner.draw(celda);
+    }
+}
 
-				if (matrix[j][i]){
-                    cout << j << ", " << i << "   |   " << j*sizeCelda_Y <<  "  -  " << i*sizeCelda_Y << endl;
-                    celda.setPosition(j*sizeCelda_Y, i*sizeCelda_Y);
-                    celda.setFillColor(sf::Color(color_vivo[0], color_vivo[1], color_vivo[2]));
-                    inner.draw(celda);
-                }
+void actionHandler(string action){
 
-                sem_post(&sem);
+    if (action == "Evolucion Automatica"){
 
-			}
-		}
+    }
+    if (action == "Siguiente Evolucion"){
+        inner.clear(sf::Color(color_muerto[0], color_muerto[1], color_muerto[2]));
+        live_cells.clear();
+        handleNextStep(0 , n - 1, 0, n - 1, 0, true);
+        matrix = matrix_next_gen;
+        matrix_next_gen = matrix_clean;
+        updateGameVisual();
+    }
 
-        cout << "Finished" << endl;
-		return;
-	}
 
-    int mitad_x = x2 / 2;
-	int mitad_y = y2 / 2;
-
-	thread cuadrantes[4];
-	
-	cuadrantes[0] = thread (updateGameVisual, x1, mitad_x, y1, mitad_y, iteracion + 1, nulo);
-	cuadrantes[1] = thread (updateGameVisual, mitad_x  + 1, x2, y1, mitad_y, iteracion + 1, nulo);
-	cuadrantes[2] = thread (updateGameVisual, x1, mitad_x, mitad_y  + 1, y2, iteracion + 1, nulo);
-	cuadrantes[3] = thread (updateGameVisual, mitad_x + 1, x2, mitad_y + 1, y2, iteracion + 1, nulo);
-
-	for (int i = 0; i < 4; i++) cuadrantes[i].join();
-    
     return;
 }
 
@@ -188,16 +174,19 @@ int main() {
 	matrix[1][2] = true;
 	matrix[2][2] = true;
 
-    // Inicializamos el semaforo para el correcto uso de los diferentes hilos sin tener problemas de frontera
-    // Se ocupo un semaforo en lugar de un mutex, ya que necesitamos que limitar el acceso concurrente a los recursos, en ente caso, 
-    // la pantalla del juego
-    sem_init(&sem, 0, 1);
+    live_cells.PB({1,0});
+    live_cells.PB({2,1});
+    live_cells.PB({0,2});
+    live_cells.PB({1,2});
+    live_cells.PB({2,2});
 
     // Creamos la ventana principal en la cual tendra todos los botones y la ventana del juego
     sf::RenderWindow outerWindow(sf::VideoMode(1650, 880), "Conway's Game of Life");
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Le asignamos las dimensiones a la pantalla del juego
     inner.create(1400, 700);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Usamos la clase Sprite para poder dibujar todas las celdas dentro de la pantalla del juego
     sf::Sprite innerSprite(inner.getTexture());
@@ -232,8 +221,8 @@ int main() {
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     // Creamos el primer estado si es que hay uno en la matriz de inicio
-    updateGameVisual(0 , n - 1, 0, n - 1, 0, true);
-
+    updateGameVisual();
+    
 
     // Bucle que ocupamos para la pantalla mientras ésta esté presente
     while (outerWindow.isOpen()) {
@@ -259,14 +248,9 @@ int main() {
                 for (int i = 0; i < buttons.size(); i++){
                     
                     if (buttons[i].first.getGlobalBounds().contains(mousePosF)) {
-                            string temp = buttons[i].second.getString();
-                            if (i == 2)
-                                inner.clear(sf::Color(color_muerto[0], color_muerto[1], color_muerto[2]));
-                                handleNextStep(0 , n - 1, 0, n - 1, 0, true);
-                                matrix = matrix_next_gen;
-                                matrix_next_gen = matrix_clean;
-                        }
-
+                            // Si el boton fue presionado, lo mandamos a nuestra funcion de actionHandler
+                            actionHandler(buttons[i].second.getString());
+                    }
                 }
           }
         }
