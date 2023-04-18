190af7eb891e3667445ff9c972720e4c64c5c7e2
Kevin F <knightkevin99@gmail.com>
2023-04-17 21:45:03 -0600

    Finally the fileDialog funciona

--- Pruebas/test1.cpp
+++ Pruebas/test1.cpp
@@ -1,46 +1,21 @@
-#include <gtk/gtk.h>
-#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <bits/stdc++.h>
+#include <gtk/gtk.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 // sudo apt-get install libgtk-3-dev
 // g++ test1.cpp `pkg-config --cflags --libs gtk+-3.0` -lsfml-graphics -lsfml-window -lsfml-system -o program
 
-void open_file() {
-    cout << "Abrimos archivo" << endl;
-
-    while(true){
-        /*
-        GtkWidget *dialog;
-        GtkFileFilter *filter;
-        GtkFileChooser *chooser;
-        GtkResponseType response;
-
-
-        gtk_init(NULL, NULL);
-        dialog = gtk_file_chooser_dialog_new("Open File", NULL, GTK_FILE_CHOOSER_ACTION_OPEN, "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
-
-        // Add a filter for text files
-        filter = gtk_file_filter_new();
-        gtk_file_filter_add_pattern(filter, "*.txt");
-        gtk_file_filter_set_name(filter, "Text files");
-        gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
-
-        response = gtk_dialog_run(GTK_DIALOG(dialog));
-
-        if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-            string filename;
-            filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-            cout << filename << endl;
-        } 
-        
-        gtk_widget_destroy(dialog);
-        */
-
-        break;
+void on_file_chooser_response(GtkDialog *dialog, gint response_id, gpointer user_data) {
+    if (response_id == GTK_RESPONSE_ACCEPT) {
+        cout << "We read the entire file" << endl;
     }
+    gtk_widget_destroy(GTK_WIDGET(dialog));
 
+    cout << "end of function" << endl;
+    gtk_main_quit(); // End the main loop and exit the program
     return;
 }
 
@@ -58,10 +33,10 @@ int main() {
 
     while (window.isOpen()) {
         sf::Event event;
+        window.clear();
+
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
+            if (event.type == sf::Event::Closed) window.close();
 
             // Check if the mouse button was pressed
             if (event.type == sf::Event::MouseButtonPressed) {
@@ -69,8 +44,30 @@ int main() {
 
                 // Check if the open button was pressed
                 if (openButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                    cout << "Open the file" << endl;
-                    open_file();
+                    cout << "Before function" << endl;
+                    window.setVisible(false);
+
+                    gtk_init(NULL, NULL);
+
+                    GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File", NULL, GTK_FILE_CHOOSER_ACTION_OPEN, "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
+                    GtkFileFilter *filter;
+
+                    // Add a filter for text files
+                    filter = gtk_file_filter_new();
+                    gtk_file_filter_add_pattern(filter, "*.txt");
+                    gtk_file_filter_set_name(filter, "Text files");
+                    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
+                    
+                    gtk_widget_show_all(dialog);
+
+                    // Connect the response signal to the on_file_chooser_response function
+                    g_signal_connect(dialog, "response", G_CALLBACK(on_file_chooser_response), NULL);
+
+                    gtk_main();
+
+                    cout << "After function" << endl;
+
+                    window.setVisible(true);
                 }
 
                 // Check if the save button was pressed
@@ -80,13 +77,13 @@ int main() {
             }
         }
 
-        window.clear();
+       
+
+
         window.draw(openButton);
         window.draw(saveButton);
         window.display();
-
-        // Sleep for a short time to avoid using too much CPU
-        this_thread::sleep_for(chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     return 0;
